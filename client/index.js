5a40ebaf0e704d60b43b58569462fe5ff22da1ef
s1ga <s.v.harlanov@gmail.com>
2023-08-07 01:33:42 +0200

    feat: implement gift list project task

--- client/index.js
+++ client/index.js
@@ -4,11 +4,16 @@ const MerkleTree = require('../utils/MerkleTree');
 
 const serverUrl = 'http://localhost:1225';
 
+const INCLUDES_NAME = 'Lester Parisian';
+const NOT_INCLUDES_NAME = 'Siarhei';
+
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  const name = INCLUDES_NAME;
+  const merkleTree = new MerkleTree(niceList);
+  const proof = merkleTree.getProof(niceList.indexOf(name));
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name, proof,
   });
 
   console.log({ gift });
